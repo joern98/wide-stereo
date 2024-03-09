401e03ea2814ec89e2e82fcd677f3f391022ddd5
joern <joern.eggersgluess@gmail.com>
2024-03-10 00:55:55 +0100

    implement first part of plane sweep (image projection)

--- plane_sweep.py
+++ plane_sweep.py
@@ -1,4 +1,7 @@
+import math
 from os import path
+from typing import Tuple, Sequence
+
 import cv2 as cv
 import numpy as np
 import argparse
@@ -32,7 +35,84 @@ def get_camera_matrix_from_dict(intrinsic_dict):
     k = [[intrinsic_dict["fx"], 0, intrinsic_dict["ppx"]],
          [0, intrinsic_dict["fy"], intrinsic_dict["ppy"]],
          [0, 0, 1]]
-    return np.asmatrix(k, dtype=np.float32)
+    return np.asanyarray(k, dtype=np.float32).reshape(3, 3)
+
+
+#       KR = get_camera_matrix_from_dict(camera_parameters["right_intrinsics"])
+#     KL = get_camera_matrix_from_dict(camera_parameters["left_intrinsics"])
+#     R = np.asmatrix(calibration_result.R_14[:3, :3])
+#     t = np.asmatrix(calibration_result.R_14[:3, 3:4]).reshape(1, 3)
+#     tn = t.T @ n
+#     H2 = KR @ (R - tn / d) @ KL.I
+def compute_homography(k_rt0: Tuple[np.ndarray, np.ndarray, np.ndarray], k_rt1: Tuple[np.ndarray, np.ndarray, np.ndarray],
+                       z: float) -> np.ndarray:
+    n = np.asarray([0, 0, -1]).reshape(3, 1)
+    K0_I = np.linalg.inv(k_rt0[0])
+    K1 = k_rt1[0]
+    R = k_rt1[1]
+    t = k_rt1[2]
+    tn = t @ n.T
+    H = K1 @ (R - tn / z) @ K0_I
+    return H
+
+
+def plane_sweep(images: [cv.Mat | np.ndarray | cv.UMat], k_rt: [Tuple[np.ndarray, np.ndarray, np.ndarray]],
+                image_size: Sequence[int],
+                z_min: float, z_max: float, z_step: float):
+    """
+    Perform the basic plane sweeping algorithm
+
+    :param images:
+    :param k_rt: (K, R, t)
+    :param image_size:
+    :param z_min:
+    :param z_max:
+    :param z_step:
+    :return:
+    """
+    n_planes = math.floor((z_max - z_min) / z_step) + 1
+    cost_volume = np.zeros((n_planes, image_size[0], image_size[1]), dtype=np.float32)
+
+    for i in range(n_planes):
+        z = z_min + i * z_step
+        _L = [images[0]]
+        for j in range(1, len(images)):
+            # L[0] is not warped, projection would only scale the image
+            H = compute_homography(k_rt[0], k_rt[j], z)
+            projected = cv.warpPerspective(images[j], H, image_size, flags=cv.WARP_INVERSE_MAP | cv.INTER_LINEAR)
+            _L.append(projected)
+        for m in range(len(_L)):
+            cv.imshow(f"Camera {m}", _L[m])
+        cv.waitKey()
+
+
+def compute_transforms(calibration_result, camera_parameters) -> [Tuple[np.ndarray, np.ndarray, np.ndarray]]:
+    R_01 = np.asarray(camera_parameters["left_stereo_extrinsics"]["r"]).reshape(3, 3)
+    t_01 = np.asarray(camera_parameters["left_stereo_extrinsics"]["t"]).reshape(3, 1)
+
+    R_12 = np.asarray(calibration_result.R)
+    t_12 = np.asarray(calibration_result.T)
+
+    R_23 = np.asarray(camera_parameters["right_stereo_extrinsics"]["r"]).reshape(3, 3)
+    t_23 = np.asarray(camera_parameters["right_stereo_extrinsics"]["t"]).reshape(3, 1)
+
+    R_02 = R_12 @ R_01
+    t_02 = R_12 @ t_01 + t_12
+
+    R_03 = R_23 @ R_02
+    t_03 = R_23 @ R_12 @ t_01 + R_23 @ t_12 + t_23
+
+    K0 = K1 = get_camera_matrix_from_dict(camera_parameters["left_intrinsics"])
+    K2 = K3 = get_camera_matrix_from_dict(camera_parameters["right_intrinsics"])
+
+    R_00 = np.eye(3)
+    t_00 = np.zeros((1, 3))
+
+    m = [(K0, R_00, t_00),
+         (K1, R_01, t_01),
+         (K2, R_02, t_02),
+         (K3, R_03, t_03)]
+    return m
 
 
 def main(args):
@@ -40,23 +120,10 @@ def main(args):
         left_ir_1, left_ir_2, right_ir_1, right_ir_2, \
         left_native_depth, right_native_depth = load_data(args.directory)
 
-    d = 2.45
-    # normal direction has to be negative z
-    n = np.asmatrix([0, 0, -1])
+    images = [left_ir_1, left_ir_2, right_ir_1, right_ir_2]
+    transforms = compute_transforms(calibration_result, camera_parameters)
 
-    KR = get_camera_matrix_from_dict(camera_parameters["right_intrinsics"])
-    KL = get_camera_matrix_from_dict(camera_parameters["left_intrinsics"])
-    R = np.asmatrix(calibration_result.R_14[:3, :3])
-    t = np.asmatrix(calibration_result.R_14[:3, 3:4]).reshape(1, 3)
-    tn = t.T @ n
-    H2 = KR @ (R - tn / d) @ KL.I
-    cv.imshow(WINDOW_LEFT_IR_1, left_ir_1)
-    cv.imshow(WINDOW_RIGHT_IR_2, right_ir_2)
-
-    # this works as well, but we need to use the inverse, i.e. instead of p1=H*p0 we have p0=H.I*p1
-    # setting the flag WARP_INVERSE_MAP to directly use the specified homography
-    warped_right = cv.warpPerspective(right_ir_2, H2, (1280, 720), flags=cv.WARP_INVERSE_MAP | cv.INTER_LINEAR)
-
+    plane_sweep(images, transforms, camera_parameters["image_size"], z_min=1.35, z_max=3, z_step=1.1)
     MOUSE_X, MOUSE_Y = 0, 0
 
     def on_mouse(event, x, y, flags, user_data):
@@ -64,18 +131,8 @@ def main(args):
         if event == cv.EVENT_MOUSEMOVE:
             MOUSE_X, MOUSE_Y = x, y
 
-    cv.setMouseCallback(WINDOW_LEFT_IR_1, on_mouse)
-
     run = True
     while run:
-        p2 = (H2 @ [MOUSE_X, MOUSE_Y, 1]).reshape(-1, 3).astype(int)
-        img = cv.copyTo(right_ir_2, None)
-        cv.drawMarker(img, (p2[0, 0], p2[0, 1]), (1, 1, 1))
-        cv.imshow(WINDOW_RIGHT_IR_2, img)
-
-        warped_right_marker = cv.copyTo(warped_right, None)
-        cv.drawMarker(warped_right_marker, (MOUSE_X, MOUSE_Y), (1, 1, 1))
-        cv.imshow(WINDOW_RIGHT_IR_1, warped_right_marker)
         key = cv.waitKey(1)
         if key == 27:  # ESCAPE
             run = False
